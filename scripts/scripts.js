d11e0eb7ccc8b0d81f60b1348540912d2f7f385a
Sahab Alfahad <sahabalfahad6@gmail.com>
2025-09-11 21:38:51 +0300

    Update scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+// Dark Mode Toggle
+const darkModeBtn = document.getElementById('darkModeBtn');
+
+darkModeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
